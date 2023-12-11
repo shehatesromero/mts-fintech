3d5bc0129f5f9b80661ba196d6c34ca806cb0da1
Staroverov_Roman <pstaroverov555555@gmail.com>
2023-12-12 00:33:46 +0300

    HW-1: Fixed bugs

--- src/main/java/ru/mts/Main.java
+++ src/main/java/ru/mts/Main.java
@@ -1,15 +1,27 @@
 package ru.mts;
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
         Price milk = new Price(2, 90, 0.75);
-        milk.calculateSum();
+        processingObject(milk);
 
         Price crisps = new Price(6, 140.95, 42.575);
-        crisps.calculateSum();
+        processingObject(crisps);
 
         Price chocolate = new Price(4, 79.99, 59.1);
-        chocolate.calculateSum();
+        processingObject(chocolate);
+    }
+
+    public static void processingObject(Price product) {
+        BigDecimal totalSumOfProducts = new BigDecimal(product.getSumOfProducts() * product.getNumberOfProducts());
+        BigDecimal Discount =
+                new BigDecimal(((product.getSumOfProducts() * product.getNumberOfProducts()) * product.getDiscountOnProduct()) / 100.0);
+        BigDecimal totalSumWithDiscount = totalSumOfProducts.subtract(Discount);
+        System.out.println("Сумма покупки: " + totalSumOfProducts.setScale(2, BigDecimal.ROUND_UP));
+        System.out.println("Сумма покупки со скидкой: " + totalSumWithDiscount.setScale(2, BigDecimal.ROUND_UP));
+
     }
 }
--- src/main/java/ru/mts/Price.java
+++ src/main/java/ru/mts/Price.java
@@ -1,10 +1,12 @@
 package ru.mts;
 
+
 /**
  * Класс Price содержит 3 параметра:
  * 1) количество товаров
  * 2) сумма товара
  * 3) скидка на товар
+ * А также имеет 2 конструктора, геттеры и сеттеры
  */
 public class Price {
 
@@ -13,21 +15,50 @@ public class Price {
     double discountOnProduct; // скидка на товар
 
     /**
-     * @param numberOfProducts Количество товаров
-     * @param sumOfProducts Сумма товаров
+     * @param numberOfProducts  Количество товаров
+     * @param sumOfProducts     Сумма товаров
      * @param discountOnProduct Скидка на товар
      */
     public Price(int numberOfProducts, double sumOfProducts, double discountOnProduct) {
+        if (numberOfProducts <= 0 || sumOfProducts <= 0) {
+            throw new IllegalArgumentException("Количество товаров и сумма товаров должны быть больше 0");
+        }
         this.numberOfProducts = numberOfProducts;
         this.sumOfProducts = sumOfProducts;
         this.discountOnProduct = discountOnProduct;
     }
 
-    public void calculateSum() {
-        double totalSumOfProducts = sumOfProducts*numberOfProducts;
-        double totalSumWithDiscount =
-                totalSumOfProducts - ((sumOfProducts*numberOfProducts)*discountOnProduct)/100.0;
-        System.out.println("Сумма покупки без скидки: " + String.format("%.2f", totalSumOfProducts));
-        System.out.println("Сумма покупки со скидкой: " + String.format("%.2f", totalSumWithDiscount));
+    public Price(int numberOfProducts, double sumOfProducts) {
+        if (numberOfProducts <= 0 || sumOfProducts <= 0) {
+            throw new IllegalArgumentException("Количество товаров и сумма товаров должны быть больше 0");
+        }
+        this.numberOfProducts = numberOfProducts;
+        this.sumOfProducts = sumOfProducts;
+    }
+
+    //Геттеры
+    public int getNumberOfProducts() {
+        return numberOfProducts;
+    }
+
+    public double getSumOfProducts() {
+        return sumOfProducts;
+    }
+
+    public double getDiscountOnProduct() {
+        return discountOnProduct;
+    }
+
+    //Сеттеры
+    public void setNumberOfProducts(int numberOfProducts) {
+        this.numberOfProducts = numberOfProducts;
+    }
+
+    public void setSumOfProducts(double sumOfProducts) {
+        this.sumOfProducts = sumOfProducts;
+    }
+
+    public void setDiscountOnProduct(double discountOnProduct) {
+        this.discountOnProduct = discountOnProduct;
     }
 }
